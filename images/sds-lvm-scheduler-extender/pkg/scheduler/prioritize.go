546dde7a6dce48d940b9c7fc8081dcff8e361ac5
viktor.kramarenko <viktor.kramarenko@flant.com>
2024-01-30 19:55:38 +0300

    [scheduler] Add scheduler-extender
    Signed-off-by: viktor.kramarenko <viktor.kramarenko@flant.com>

--- images/sds-lvm-scheduler-extender/pkg/scheduler/prioritize.go
+++ images/sds-lvm-scheduler-extender/pkg/scheduler/prioritize.go
@@ -1,25 +1,101 @@
 package scheduler
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	corev1 "k8s.io/api/core/v1"
 	"math"
 	"net/http"
-	"strconv"
-	"strings"
+	"sds-lvm-scheduler-extender/api/v1alpha1"
+	"sds-lvm-scheduler-extender/pkg/logger"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sync"
-
-	"github.com/topolvm/topolvm"
-	corev1 "k8s.io/api/core/v1"
 )
 
-func capacityToScore(capacity uint64, divisor float64) int {
+func (s scheduler) prioritize(w http.ResponseWriter, r *http.Request) {
+	s.log.Debug("[prioritize] starts serving")
+	var input ExtenderArgs
+	reader := http.MaxBytesReader(w, r.Body, 10<<20)
+	err := json.NewDecoder(reader).Decode(&input)
+	if err != nil {
+		s.log.Error(err, "[prioritize] unable to decode a request")
+		http.Error(w, "Bad Request.", http.StatusBadRequest)
+		return
+	}
+
+	result, err := scoreNodes(s.client, s.log, input.Nodes.Items, s.defaultDivisor)
+	if err != nil {
+		s.log.Error(err, "[prioritize] unable to score nodes")
+		http.Error(w, "Bad Request.", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("content-type", "application/json")
+	err = json.NewEncoder(w).Encode(result)
+	if err != nil {
+		s.log.Error(err, "[prioritize] unable to encode a response")
+		http.Error(w, "internal error", http.StatusInternalServerError)
+	}
+	s.log.Debug("[prioritize] ends serving")
+}
+
+func scoreNodes(cl client.Client, log logger.Logger, nodes []corev1.Node, divisor float64) ([]HostPriority, error) {
+	ctx := context.Background()
+	lvgl := &v1alpha1.LvmVolumeGroupList{}
+	err := cl.List(ctx, lvgl)
+	if err != nil {
+		return nil, err
+	}
+
+	lvgByNodes := make(map[string][]v1alpha1.LvmVolumeGroup, len(lvgl.Items))
+	for _, lvg := range lvgl.Items {
+		for _, node := range lvg.Status.Nodes {
+			lvgByNodes[node.Name] = append(lvgByNodes[node.Name], lvg)
+		}
+	}
+
+	log.Trace(fmt.Sprintf("[scoreNodes] sorted LVG by nodes: %+v", lvgByNodes))
+
+	result := make([]HostPriority, 0, len(nodes))
+
+	// TODO: probably should score the nodes exactly to their free space
+	wg := &sync.WaitGroup{}
+	wg.Add(len(nodes))
+
+	for _, node := range nodes {
+		go func(node corev1.Node) {
+			defer wg.Done()
+
+			lvgs := lvgByNodes[node.Name]
+			freeSpace, err := getNodeFreeSpace(lvgs)
+			if err != nil {
+				log.Error(err, fmt.Sprintf("[scoreNodes] unable to get node free space, node: %s, lvgs: %+v", node.Name, lvgs))
+				return
+			}
+
+			score := getNodeScore(freeSpace, divisor)
+			result = append(result, HostPriority{Host: node.Name, Score: score})
+		}(node)
+	}
+	wg.Wait()
+
+	for _, n := range result {
+		log.Trace(fmt.Sprintf("[scoreNodes] host: %s", n.Host))
+		log.Trace(fmt.Sprintf("[scoreNodes] score: %d", n.Score))
+	}
+
+	return result, nil
+}
+
+func getNodeScore(freeSpace map[string]int64, divisor float64) int {
+	capacity := freeSpace[thin] + freeSpace[thick]
 	gb := capacity >> 30
 
 	// Avoid logarithm of zero, which diverges to negative infinity.
 	if gb == 0 {
-		// If there is a non-nil capacity but we dont have at least one gigabyte, we score it with one.
+		// If there is a non-nil capacity, but we don't have at least one gigabyte, we score it with one.
 		// This is because the capacityToScore precision is at the gigabyte level.
-		// TODO: introduce another scheduling algorithm for byte-level precision.
 		if capacity > 0 {
 			return 1
 		}
@@ -36,71 +112,4 @@ func capacityToScore(capacity uint64, divisor float64) int {
 	default:
 		return converted
 	}
-}
-
-func scoreNodes(pod *corev1.Pod, nodes []corev1.Node, defaultDivisor float64, divisors map[string]float64) []HostPriority {
-	var dcs []string
-	for k := range pod.Annotations {
-		if strings.HasPrefix(k, topolvm.GetCapacityKeyPrefix()) {
-			dcs = append(dcs, k[len(topolvm.GetCapacityKeyPrefix()):])
-		}
-	}
-	if len(dcs) == 0 {
-		return nil
-	}
-
-	result := make([]HostPriority, len(nodes))
-	wg := &sync.WaitGroup{}
-	wg.Add(len(nodes))
-	for i := range nodes {
-		r := &result[i]
-		item := nodes[i]
-		go func() {
-			score := scoreNode(item, dcs, defaultDivisor, divisors)
-			*r = HostPriority{Host: item.Name, Score: score}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-
-	return result
-}
-
-func scoreNode(item corev1.Node, deviceClasses []string, defaultDivisor float64, divisors map[string]float64) int {
-	minScore := math.MaxInt32
-	for _, dc := range deviceClasses {
-		if val, ok := item.Annotations[topolvm.GetCapacityKeyPrefix()+dc]; ok {
-			capacity, _ := strconv.ParseUint(val, 10, 64)
-			var divisor float64
-			if v, ok := divisors[dc]; ok {
-				divisor = v
-			} else {
-				divisor = defaultDivisor
-			}
-			score := capacityToScore(capacity, divisor)
-			if score < minScore {
-				minScore = score
-			}
-		}
-	}
-	if minScore == math.MaxInt32 {
-		minScore = 0
-	}
-	return minScore
-}
-
-func (s scheduler) prioritize(w http.ResponseWriter, r *http.Request) {
-	var input ExtenderArgs
-
-	reader := http.MaxBytesReader(w, r.Body, 10<<20)
-	err := json.NewDecoder(reader).Decode(&input)
-	if err != nil {
-		http.Error(w, "Bad Request.", http.StatusBadRequest)
-		return
-	}
-
-	result := scoreNodes(input.Pod, input.Nodes.Items, s.defaultDivisor, s.divisors)
-
-	w.Header().Set("content-type", "application/json")
-	json.NewEncoder(w).Encode(result)
 }
--- images/sds-lvm-scheduler-extender/pkg/scheduler/route.go
+++ images/sds-lvm-scheduler-extender/pkg/scheduler/route.go
@@ -3,37 +3,45 @@ package scheduler
 import (
 	"fmt"
 	"net/http"
+	"sds-lvm-scheduler-extender/pkg/logger"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type scheduler struct {
 	defaultDivisor float64
-	divisors       map[string]float64
+	log            logger.Logger
+	client         client.Client
 }
 
 func (s scheduler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
-	case "/predicate":
-		s.predicate(w, r)
+	case "/filter":
+		s.log.Debug("[ServeHTTP] filter route starts handling the request")
+		s.filter(w, r)
+		s.log.Debug("[ServeHTTP] filter route ends handling the request")
 	case "/prioritize":
+		s.log.Debug("[ServeHTTP] prioritize route starts handling the request")
 		s.prioritize(w, r)
+		s.log.Debug("[ServeHTTP] prioritize route ends handling the request")
 	case "/status":
+		s.log.Debug("[ServeHTTP] status route starts handling the request")
 		status(w, r)
+		s.log.Debug("[ServeHTTP] status route ends handling the request")
 	default:
 		http.Error(w, "not found", http.StatusNotFound)
 	}
 }
 
 // NewHandler return new http.Handler of the scheduler extender
-func NewHandler(defaultDiv float64, divisors map[string]float64) (http.Handler, error) {
-	for _, divisor := range divisors {
-		if divisor <= 0 {
-			return nil, fmt.Errorf("invalid divisor: %f", divisor)
-		}
-	}
-	return scheduler{defaultDiv, divisors}, nil
+func NewHandler(cl client.Client, log logger.Logger, defaultDiv float64) (http.Handler, error) {
+	return scheduler{defaultDiv, log, cl}, nil
 }
 
 func status(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ok"))
+	_, err := w.Write([]byte("ok"))
+	if err != nil {
+		fmt.Println(fmt.Sprintf("error occurs on status route, err: %s", err.Error()))
+	}
 }
