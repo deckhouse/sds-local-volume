963be45d0cfb9ba8a73cdace7461c8f40b6319ff
v.oleynikov <vasily.oleynikov@flant.com>
2024-05-27 10:53:02 +0300

    Fixes in CI
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- .github/check_previous_channel_release.sh
+++ .github/check_previous_channel_release.sh
@@ -5,6 +5,8 @@ repositoryName=$1
 edition=$2
 channel=$3
 version=$4
+user=$5
+password=$6
 
 echo "Module $repositoryName, edition $edition, channel $channel, version $version"
 
@@ -25,6 +27,7 @@ else
 fi
 
 echo "Checking previous channel $previousChannel"
+crane auth login -u $user -p $password registry.deckhouse.io
 previousChannelVersion=$($crane export registry.deckhouse.io/deckhouse/$edition/modules/$repositoryName/release:$previousChannel | grep -aoE '\{"version":".*"\}' | jq -r .version)
 if [[ "$version" == "$previousChannelVersion" ]]; then
  echo "Previous channel $previousChannel version $previousChannelVersion is equal desired version $version, processing"
