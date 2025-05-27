8bb60e117b2fa7694f955fe0807037c3af08cbd4
Nikolay Demchuk <nikolay.demchuk@flant.com>
2025-05-28 00:31:32 +1000

    fix Signed-off-by: Nikolay Demchuk <nikolay.demchuk@flant.com>

--- hooks/go/030-enable-thin-provisioning/thin_provisioning.go
+++ hooks/go/030-enable-thin-provisioning/thin_provisioning.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2025 Flant JSC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package thinprovisioning
 
 import (
@@ -21,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"gopkg.in/yaml.v3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -33,51 +18,50 @@ const (
 	crdGroup      = "storage.deckhouse.io"
 	crdNamePlural = "localstorageclasses"
 	crdVersion    = "v1alpha1"
+	configVersion = "v1"
+	afterHelm     = 10
 )
 
 func init() {
+	// Load in-cluster Kubernetes configuration
 	config, err := rest.InClusterConfig()
 	if err != nil {
+		// Fallback to kubeconfig for local testing
 		kubeconfig := os.Getenv("KUBECONFIG")
 		if kubeconfig == "" {
 			kubeconfig = os.Getenv("HOME") + "/.kube/config"
 		}
 		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
 		if err != nil {
-			_, err := fmt.Fprintf(os.Stderr, "Failed to load Kubernetes config: %v\n", err)
-			if err != nil {
-				return
-			}
+			fmt.Fprintf(os.Stderr, "Failed to load Kubernetes config: %v\n", err)
 			os.Exit(1)
 		}
 	}
 
+	// Create dynamic client for interacting with custom resources
 	dynamicClient, err := dynamic.NewForConfig(config)
 	if err != nil {
-		_, err := fmt.Fprintf(os.Stderr, "Failed to create dynamic client: %v\n", err)
-		if err != nil {
-			return
-		}
+		fmt.Fprintf(os.Stderr, "Failed to create dynamic client: %v\n", err)
 		os.Exit(1)
 	}
 
+	// Define the custom resource GVR (Group, Version, Resource)
 	localStorageClassGVR := schema.GroupVersionResource{
 		Group:    crdGroup,
 		Version:  crdVersion,
 		Resource: crdNamePlural,
 	}
 
+	// List localstorageclasses custom resources
 	list, err := dynamicClient.Resource(localStorageClassGVR).List(context.Background(), metav1.ListOptions{})
 	if err != nil {
-		_, err := fmt.Fprintf(os.Stderr, "Failed to list custom objects: %v\n", err)
-		if err != nil {
-			return
-		}
+		fmt.Fprintf(os.Stderr, "Failed to list custom objects: %v\n", err)
 		os.Exit(1)
 	}
 
 	thinPoolExistence := false
 	for _, item := range list.Items {
+		// Extract spec.lvm.type
 		lvmType, found, err := unstructured.NestedString(item.Object, "spec", "lvm", "type")
 		if err != nil || !found {
 			continue
@@ -88,8 +72,9 @@ func init() {
 		}
 	}
 
+	// If thinPoolExistence is true, apply YAML configuration to moduleconfigs
 	if thinPoolExistence {
-		_, err := dynamicClient.Resource(schema.GroupVersionResource{
+		resp, err := dynamicClient.Resource(schema.GroupVersionResource{
 			Group:    "deckhouse.io",
 			Version:  "v1alpha1",
 			Resource: "moduleconfigs",
@@ -102,13 +87,14 @@ func init() {
 			},
 		}, metav1.ApplyOptions{FieldManager: "sds-hook"})
 		if err != nil {
-			_, err := fmt.Fprintf(os.Stderr, "Failed to apply YAML configuration: %v\n", err)
-			if err != nil {
-				return
-			}
+			fmt.Fprintf(os.Stderr, "Failed to apply YAML configuration: %v\n", err)
 			os.Exit(1)
 		}
-
-		fmt.Println("Thin pools present, switching enableThinProvisioning on")
+		yamlResp, err := yaml.Marshal(resp.Object)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to marshal response to YAML: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Thin pools present, switching enableThinProvisioning on\n---\n%s\n", yamlResp)
 	}
 }
