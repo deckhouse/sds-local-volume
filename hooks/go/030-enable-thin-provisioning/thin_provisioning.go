4b4401e0440015cd62caa456bc1b08bfebab2d1e
Nikolay Demchuk <nikolay.demchuk@flant.com>
2025-05-27 19:19:34 +1000

    fix Signed-off-by: Nikolay Demchuk <nikolay.demchuk@flant.com>

--- hooks/go/030-enable-thin-provisioning/thin_provisioning.go
+++ hooks/go/030-enable-thin-provisioning/thin_provisioning.go
@@ -98,7 +98,7 @@ func init() {
 		_, err := dynamicClient.Resource(moduleConfigGVR).Patch(
 			context.Background(),
 			"sds-local-volume",
-			"application/merge-patch+yaml",
+			"application/apply-patch+yaml",
 			[]byte(`{"spec":{"version":1,"settings":{"enableThinProvisioning":true}}}`),
 			metav1.PatchOptions{},
 		)
