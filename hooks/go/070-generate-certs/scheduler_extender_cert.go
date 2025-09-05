dffd5bff8f3d510a5d01ba87c92a0fbe93ac6307
Nikolay Demchuk <nikolay.demchuk@flant.com>
2025-09-05 17:16:33 +1000

    test Signed-off-by: Nikolay Demchuk <nikolay.demchuk@flant.com>
    Signed-off-by: Nikolay Demchuk <nikolay.demchuk@flant.com>

--- hooks/go/070-generate-certs/scheduler_extender_cert.go
+++ hooks/go/070-generate-certs/scheduler_extender_cert.go
@@ -5,7 +5,11 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
+<<<<<<< HEAD
 	http://www.apache.org/licenses/LICENSE-2.0
+=======
+    http://www.apache.org/licenses/LICENSE-2.0
+>>>>>>> a2129d5 (test)
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +17,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+<<<<<<< HEAD
 package generatecerts
 
 import (
@@ -36,3 +41,13 @@ var _ = tlscertificate.RegisterInternalTLSHookEM(tlscertificate.GenSelfSignedTLS
 	}),
 	FullValuesPathPrefix: fmt.Sprintf("%s.internal.customSchedulerExtenderCert", consts.ModuleName),
 })
+=======
+
+package generatecerts
+
+import "github.com/deckhouse/csi-nfs/hooks/go/certs"
+
+func init() {
+	certs.RegisterSchedulerExtenderCertHook()
+}
+>>>>>>> a2129d5 (test)
